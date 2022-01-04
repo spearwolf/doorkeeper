7575d26b0bbde3b011fa29b2bc1b49d6e5e88b26
Wolfger Schramm <wolfger@spearwolf.de>
2022-01-04 16:05:49 +0100

    support npm test -- with custom arguments

--- scripts/runTests.js
+++ scripts/runTests.js
@@ -4,5 +4,10 @@ import "./setupProjectEnv.js";
 export default async (nodeEnv = "test") => {
   process.env.NODE_ENV = nodeEnv;
 
+  if (process.argv.length > 3) {
+    const customArgs = Array.from(process.argv).slice(3);
+    return npmExec("mocha", "--colors", ...customArgs);
+  }
+
   return npmExec("mocha", "--colors", "test/*");
 };
